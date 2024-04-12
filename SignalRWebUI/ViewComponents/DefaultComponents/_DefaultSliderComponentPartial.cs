a4ec29bac9c2b57d72b9ae28a50cf5e91be99b27
Eren Çolak <eren.colk01@gmail.com>
2024-04-12 13:50:30 +0300

    Sliderdaki veriler comsume edildi

--- SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultSliderComponentPartial.cs
@@ -1,13 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.DtoLayer.SliderDtos;
 
 namespace SignalRWebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultSliderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultSliderComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
 
-            return View();
+            var client = _httpClientFactory.CreateClient(); // Bir istemci yarat 
+            var responseMessage = await client.GetAsync("https://localhost:7194/api/Sliders"); //Tetiklenecek yer 
+            var jsonData = await responseMessage.Content.ReadAsStringAsync(); // Jsondan gelen içeriği string formatında oku 
+            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData); // Burada okunan değeri Json Formatından deserilaze ederek normal metne döndürür.
+            return View(values);
+
+
         }
     }
 }
