bfdd8004415743fc409b683270fae77a078710db
Eren Çolak <eren.colk01@gmail.com>
2024-04-14 16:59:17 +0300

    Menu table layout ve basket classı eklendi

--- SignalR.EntityLayer/Entities/Product.cs
+++ SignalR.EntityLayer/Entities/Product.cs
@@ -17,5 +17,6 @@
         public int CategoryID { get; set; }
         public Category Category { get; set; }
         public List<OrderDetail> OrderDetails { get; set; }
+        public List<Basket> Baskets { get; set; }
     }
 }
---
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+
+            var client = _httpClientFactory.CreateClient(); // Bir istemci yarat 
+            var responseMessage = await client.GetAsync("https://localhost:7194/api/Product"); //Tetiklenecek yer 
+            var jsonData = await responseMessage.Content.ReadAsStringAsync(); // Jsondan gelen içeriği string formatında oku 
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData); // Burada okunan değeri Json Formatından deserilaze ederek normal metne döndürür.
+            return View(values);
+
+
+        }
+    }
+}
