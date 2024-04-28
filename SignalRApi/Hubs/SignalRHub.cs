c23df647ec2ebef69f8948852e7c24f325851410
Eren Çolak <eren.colk01@gmail.com>
2024-04-28 15:33:00 +0300

    mesajlaşma kısmının js tarafı bitti

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -121,6 +121,11 @@ namespace SignalRApi.Hubs
 
         }
 
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
 
     }
 }
