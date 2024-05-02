a8605302202f67f9d6651b5f1467ffe528a65de6
Eren Çolak <eren.colk01@gmail.com>
2024-05-02 22:02:23 +0300

    Referans ve bildirimler düzeltildi

--- SignalR.DtoLayer/TestimonialDto/UpdateTestimonialDto.cs
+++ SignalR.DtoLayer/TestimonialDto/UpdateTestimonialDto.cs
@@ -8,11 +8,11 @@ namespace SignalR.DtoLayer.TestimonialDto
 {
 	public class UpdateTestimonialDto
 	{
-		public int TestimonialID { get; set; }
-		public string Name { get; set; }
-		public string Title { get; set; }
-		public string Comment { get; set; }
-		public string ImageUrl { get; set; }
-		public bool Status { get; set; }
-	}
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
 }
