0bfceb5cda411afa6f87286f5340d37800e1d6dc
Eren Çolak <eren.colk01@gmail.com>
2024-05-07 10:49:20 +0300

    Mail Gönderme metodu yazıldı

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,50 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUI.Dtos.MailDto;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MailController : Controller
+    {
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(CreateMailDto createMailDto)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon","eren.colk01@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", createMailDto.ReceiverMail);
+            mimeMessage.From.Add(mailboxAddressTo);
+
+
+            var bodyBuilder = new BodyBuilder();    
+            bodyBuilder.TextBody = createMailDto.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = createMailDto.Subject;
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com",587,false);
+            smtpClient.Authenticate("eren.colk01@gmail.com", "lkzb krhq qsyg vxmt");
+
+
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+                
+
+
+            return RedirectToAction("Index","Category");
+        }
+    }
+}
---
+++ SignalRWebUI/Dtos/MailDto/CreateMailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRWebUI.Dtos.MailDto
+{
+    public class CreateMailDto
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
